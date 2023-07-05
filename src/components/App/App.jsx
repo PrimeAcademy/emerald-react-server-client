233ef21fc01768912fe006de4ba997ceec08e03c
Emma Stout <emma@primeacademy.io>
2023-07-05 11:34:02 -0500

    complete GET request with axios

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,7 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
+// import React from 'react';
+// const useState = React.useState;
+import axios from 'axios';
 
 function App () {
  
@@ -8,6 +11,32 @@ function App () {
     {name: 'Jackalope', origin: 'America'}
   ]);
   
+  const fetchCreatures = () => {
+    /* Alternative syntax for axios call:
+    axios.get('/creature')
+    .then(...)
+    */
+    axios({
+      method: 'GET',
+      url: '/creature'
+    }).then( response => {
+      // response.data is where the server response data we care about is
+      console.log('response is: ', response.data);
+      setCreatureList(response.data);
+    }).catch(error =>{
+      console.log('error with fetchCreatures: ', error);
+    });
+  }
+
+  // Hook into the React lifecycle and run fetchCreatures when
+  // this component loads 
+  // React equivalent to jQuery's onReady()
+  useEffect(() => {
+    fetchCreatures();
+  }, []);
+  // ^ The empty array is saying that this should only run once, 
+  // when the component loads
+
   return (
     <div>
       <ul>
